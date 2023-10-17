106e92e32b0a65d674761c975b00b840d9ae759c
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2023-10-17 13:29:17 +0200

    After meeting fixes

--- GolemLib/job.cs
+++ GolemLib/job.cs
@@ -10,6 +10,10 @@ public interface Job : INotifyPropertyChanged
     /// </summary>
     public string Id { get; init; }
     /// <summary>
+    /// Requestor Node identification in Golem network.
+    /// </summary>
+    public string RequestorId { get; }
+    /// <summary>
     /// Price vector for which this job was initialized.
     /// Note that even if you change `IGolem.Price` this field won't be
     /// affected, only new jobs will be served using new price.
