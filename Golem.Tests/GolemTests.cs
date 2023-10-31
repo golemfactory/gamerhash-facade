69ec3ed71d74ebbbee283aed6fd5584994b4f9f6
Przemyslaw Walski <pwalski@users.noreply.github.com>
2023-10-31 11:37:21 +0100

    Test patched up to start-up provider

--- Golem.Tests/GolemTests.cs
+++ Golem.Tests/GolemTests.cs
@@ -7,7 +7,7 @@ namespace Golem.Tests
 {
     public class GolemTests
     {
-        string golemPath = "c:\\git\\yagna\\target\\debug";
+        string golemPath = "d:\\code\\yagna\\target\\debug";
 
         [Fact]
         public async Task StartStop_VerifyStatusAsync()
@@ -26,6 +26,9 @@ namespace Golem.Tests
             await golem.Start();
 
             Assert.Equal(GolemStatus.Ready, status);
+
+            Console.WriteLine("Sleep for 1000 seconds.");
+            Thread.Sleep(1_000_000);
 
             await golem.Stop();
 
