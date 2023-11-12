1bd68fb4d7c494f7c0244a82db3fc67d409b24d5
Przemyslaw Walski <pwalski@users.noreply.github.com>
2023-11-12 07:17:42 +0100

    Activity loop refactor

--- Golem/Golem.cs
+++ Golem/Golem.cs
@@ -23,12 +23,12 @@ namespace Golem
         private YagnaService Yagna { get; set; }
         private Provider Provider { get; set; }
 
-        private Task? activityLoop;
+        private Task? _activityLoop;
 
         private readonly ILogger? _logger;
         private readonly CancellationTokenSource _tokenSource;
 
-        private readonly HttpClient HttpClient;
+        private readonly HttpClient _httpClient;
 
         public GolemPrice Price { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public string WalletAddress { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
@@ -36,8 +36,8 @@ namespace Golem
 
 
         private GolemStatus status;
-        private string golemPath;
-        private ILoggerFactory loggerFactory;
+        private readonly string golemPath;
+        private readonly ILoggerFactory loggerFactory;
 
         public GolemStatus Status
         {
@@ -120,7 +120,7 @@ namespace Golem
             throw new NotImplementedException();
         }
 
-        public Golem(string golemPath, string? dataDir, ILoggerFactory? loggerFactory=null)
+        public Golem(string golemPath, string? dataDir, ILoggerFactory? loggerFactory = null)
         {
             var prov_datadir = Path.Combine(dataDir, "provider");
             var yagna_datadir = Path.Combine(dataDir, "yagna");
@@ -131,7 +131,7 @@ namespace Golem
             Yagna = new YagnaService(golemPath, yagna_datadir, loggerFactory);
             Provider = new Provider(golemPath, prov_datadir, loggerFactory);
 
-            HttpClient = new HttpClient
+            _httpClient = new HttpClient
             {
                 BaseAddress = new Uri(YagnaOptionsFactory.DefaultYagnaApiUrl)
             };
@@ -153,11 +153,12 @@ namespace Golem
                 Yagna.BindOutputDataReceivedEvent(OnYagnaOutputDataRecv);
             }
 
-            HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", yagnaOptions.AppKey);
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", yagnaOptions.AppKey);
 
             Thread.Sleep(700);
 
-            this.activityLoop = StartActivityLoop();
+            //TODO what if activityLoop != null?
+            this._activityLoop = StartActivityLoop();
 
             //yagna is starting and /me won't work until all services are running
             for (int tries = 0; tries < 300; ++tries)
@@ -171,7 +172,7 @@ namespace Golem
 
                 try
                 {
-                    var response = HttpClient.GetAsync($"/me").Result;
+                    var response = _httpClient.GetAsync($"/me").Result;
                     if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
                     {
                         throw new Exception("Unauthorized call to yagna daemon - is another instance of yagna running?");
@@ -239,8 +240,6 @@ namespace Golem
             Console.WriteLine($"[Data]: {e.Data}");
         }
 
-
-
         private class TrackingEvent
         {
             public DateTime Ts { get; set; }
@@ -248,121 +247,23 @@ namespace Golem
             public List<ActivityState> Activities { get; set; } = new List<ActivityState>();
         }
 
-        private async Task StartActivityLoop()
+        private Task StartActivityLoop()
         {
-            _logger?.LogInformation("Starting monitoring activities");
             var token = _tokenSource.Token;
-
-            var options = new JsonSerializerOptions()
-            {
-                PropertyNameCaseInsensitive = true,
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
-            };
-
-            token.Register(HttpClient.CancelPendingRequests);
-
-            DateTime newReconnect = DateTime.Now;
-            while (!token.IsCancellationRequested)
-            {
-                _logger?.LogInformation("Monitoring activities");
-                var now = DateTime.Now;
-                if (newReconnect > now)
-                {
-                    await Task.Delay(newReconnect - now);
-                }
-                newReconnect = now + TimeSpan.FromMinutes(2);
-                if (token.IsCancellationRequested)
-                {
-                    token.ThrowIfCancellationRequested();
-                }
-
-                try
-                {
-                    var stream = await HttpClient.GetStreamAsync("/activity-api/v1/_monitor");
-                    using StreamReader reader = new StreamReader(stream);
-                    token.Register(() =>
-                    {
-                        _logger?.LogInformation("stop");
-                        reader.Close();
-                    });
-                    StringBuilder dataBuilder = new StringBuilder();
-                    while (true)
-                    {
-                        if (token.IsCancellationRequested)
-                        {
-                            token.ThrowIfCancellationRequested();
-                        }
-                        var line = await reader.ReadLineAsync();
-                        if (line.StartsWith("data:"))
-                        {
-                            dataBuilder.Append(line.Substring(5).TrimStart());
-                            _logger?.LogInformation("got line {0}", line);
-                        }
-                        else if (line == "")
-                        {
-                            var json = dataBuilder.ToString();
-                            dataBuilder.Clear();
-                            _logger?.LogInformation("got json {0}", json);
-                            try
-                            {
-                                var @event = JsonSerializer.Deserialize<TrackingEvent>(json, options);
-                                var _activities = @event?.Activities ?? new List<ActivityState>();
-                                if (!_activities.Any())
-                                {
-                                    _logger?.LogInformation("No activities");
-                                    EmitJobEvent(null);
-                                    continue;
-                                }
-                                var _active_activities = _activities.FindAll(activity => activity.State != ActivityState.StateType.Terminated);
-                                if (!_active_activities.Any())
-                                {
-                                    _logger?.LogInformation("All activities terminated: {}", _activities);
-                                    EmitJobEvent(null);
-                                    continue;
-                                }
-                                if (_active_activities.Count > 1)
-                                {
-                                    _logger?.LogWarning("Multiple non terminated activities: {}", _active_activities);
-                                    //TODO what now?
-                                }
-                                //TODO take latest? the one with specific status?
-                                ActivityState _activity = _activities.First();
-                                if (_activity.AgreementId == null)
-                                {
-                                    _logger?.LogInformation("Activity without agreement id: {}", _activity);
-                                    EmitJobEvent(null);
-                                    continue;
-                                }
-                                var new_job = new Job() { Id = _activity.AgreementId };
-                                EmitJobEvent(new_job);
-                            }
-                            catch (JsonException e)
-                            {
-                                _logger?.LogError(e, "Invalid monitoring event: {0}", json);
-                                break;
-                            }
-                        }
-                    }
-                }
-                catch (HttpRequestException e)
-                {
-                    _logger?.LogError(e, "failed to get exe-units status");
-                }
-                catch (IOException e)
-                {
-                    _logger?.LogError(e, "status loop failure");
-                }
-            }
+            token.Register(_httpClient.CancelPendingRequests);
+            return new ActivityLoop(this, token, _logger).Start();
         }
 
         private void EmitJobEvent(Job? job)
         {
-            if (CurrentJob != job) {
+            if (CurrentJob != job)
+            {
                 CurrentJob = job;
                 _logger?.LogInformation("New job: {}", job);
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentJob)));
-            } else {
+            }
+            else
+            {
                 _logger?.LogInformation("Job has not changed.");
             }
         }
@@ -371,7 +272,7 @@ namespace Golem
         {
             try
             {
-                var txt = await HttpClient.GetStringAsync($"/market-api/v1/agreements/{agreementID}");
+                var txt = await _httpClient.GetStringAsync($"/market-api/v1/agreements/{agreementID}");
                 YagnaAgreement? aggr = JsonSerializer.Deserialize<YagnaAgreement>(txt) ?? null;
                 return aggr;
             }
@@ -385,6 +286,140 @@ namespace Golem
         public async ValueTask DisposeAsync()
         {
             await (this as IGolem).Stop();
+        }
+
+
+        class ActivityLoop
+        {
+
+            private const string _dataPrefix = "data:";
+            private static readonly TimeSpan s_reconnectDelay = TimeSpan.FromSeconds(120);
+            private static readonly JsonSerializerOptions s_serializerOptions = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+            };
+
+            private readonly Golem _golem;
+            private readonly CancellationToken _token;
+            private readonly ILogger _logger;
+
+            public ActivityLoop(Golem golem, CancellationToken token, ILogger logger)
+            {
+                _golem = golem;
+                _token = token;
+                _logger = logger;
+            }
+
+            public async Task Start()
+            {
+                _logger?.LogInformation("Starting monitoring activities");
+
+                DateTime newReconnect = DateTime.Now;
+                while (!_token.IsCancellationRequested)
+                {
+                    _logger?.LogInformation("Monitoring activities");
+                    var now = DateTime.Now;
+                    if (newReconnect > now)
+                    {
+                        await Task.Delay(newReconnect - now);
+                    }
+                    newReconnect = now + s_reconnectDelay;
+                    if (_token.IsCancellationRequested)
+                    {
+                        _token.ThrowIfCancellationRequested();
+                    }
+
+                    try
+                    {
+                        var stream = await _golem._httpClient.GetStreamAsync("/activity-api/v1/_monitor");
+                        using StreamReader reader = new StreamReader(stream);
+
+                        await foreach (string json in EnumerateMessages(reader).WithCancellation(_token))
+                        {
+                            _logger?.LogInformation("got json {0}", json);
+                            try
+                            {
+                                var trackingEvent = JsonSerializer.Deserialize<TrackingEvent>(json, s_serializerOptions);
+                                var _activities = trackingEvent?.Activities ?? new List<ActivityState>();
+                                if (!_activities.Any())
+                                {
+                                    _logger?.LogInformation("No activities");
+                                    _golem.EmitJobEvent(null);
+                                    continue;
+                                }
+                                var _active_activities = _activities.FindAll(activity => activity.State != ActivityState.StateType.Terminated);
+                                if (!_active_activities.Any())
+                                {
+                                    _logger?.LogInformation("All activities terminated: {}", _activities);
+                                    _golem.EmitJobEvent(null);
+                                    continue;
+                                }
+                                if (_active_activities.Count > 1)
+                                {
+                                    _logger?.LogWarning("Multiple non terminated activities: {}", _active_activities);
+                                    //TODO what now?
+                                }
+                                //TODO take latest? the one with specific status?
+                                ActivityState _activity = _activities.First();
+                                if (_activity.AgreementId == null)
+                                {
+                                    _logger?.LogInformation("Activity without agreement id: {}", _activity);
+                                    _golem.EmitJobEvent(null);
+                                    continue;
+                                }
+                                var new_job = new Job() { Id = _activity.AgreementId };
+                                _golem.EmitJobEvent(new_job);
+                            }
+                            catch (JsonException e)
+                            {
+                                _logger?.LogError(e, "Invalid monitoring event: {0}", json);
+                                break;
+                            }
+
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        _logger?.LogError(e, "status loop failure");
+                    }
+
+
+                }
+            }
+
+            private async IAsyncEnumerable<String> EnumerateMessages(StreamReader reader)
+            {
+                StringBuilder messageBuilder = new StringBuilder();
+                while (true)
+                {
+                    try
+                    {
+                        String line;
+                        while (!String.IsNullOrEmpty(line = await reader.ReadLineAsync()))
+                        {
+                            if (line.StartsWith(_dataPrefix))
+                            {
+                                messageBuilder.Append(line.Substring(_dataPrefix.Length).TrimStart());
+                                _logger?.LogInformation("got line {0}", line);
+                            }
+                            else
+                            {
+                                _logger?.LogError("Unable to deserialize message: {}", line);
+                            }
+                        }
+                    }
+                    catch (Exception error)
+                    {
+                        _logger?.LogError("Failed to read message: {}", error);
+                        break;
+                    }
+                    yield return messageBuilder.ToString();
+                    messageBuilder.Clear();
+                }
+                yield break;
+            }
         }
     }
 }
