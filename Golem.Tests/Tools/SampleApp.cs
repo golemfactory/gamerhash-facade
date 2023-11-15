9b47bdbb40b8878ccde6d14417e4e31898ebc417
Przemysław Walski <pwalski@users.noreply.github.com>
2023-11-15 11:24:25 +0100

    Build of sample AI app

---
+++ Golem.Tests/Tools/SampleApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+using Xunit.Abstractions;
+
+namespace App {
+    public class SampleApp {
+
+        public void Start() {
+            // var engine = Python.CreateEngine();
+            // ICollection<string> searchPaths = engine.GetSearchPaths();
+            // searchPaths.Add("py\\venv\\Lib");
+            // searchPaths.Add("py\\venv\\Lib\\site-packages");
+            // engine.SetSearchPaths(searchPaths);
+            // var scope = engine.ExecuteFile("py/app.py");
+            // dynamic main = scope.GetVariable("main");
+            // var x = main();
+        }
+    }
+}
