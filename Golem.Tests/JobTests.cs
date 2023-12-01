f0020befe7f2e61ff0f37b0530e896c78e148eb8
Przemysław Walski <pwalski@users.noreply.github.com>
2023-12-01 02:22:47 +0100

    JobTests observing all properties change events

--- Golem.Tests/JobTests.cs
+++ Golem.Tests/JobTests.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.Threading.Channels;
 
 using App;
@@ -68,51 +69,88 @@ namespace Golem.Tests
         }
 
         [Fact]
-        public async Task StartStop_Job()
+        public async Task CompleteScenario()
         {
+            // Having
+
             string golemPath = await PackageBuilder.BuildTestDirectory(nameof(JobTests));
             _logger.LogInformation($"Path: {golemPath}");
             var golem = new Golem(PackageBuilder.BinariesDir(golemPath), PackageBuilder.DataDir(golemPath), _loggerFactory);
 
-            var statusChannel = Channel.CreateUnbounded<GolemStatus>();
-            Action<GolemStatus> golemStatus = async (v) =>
-            {
-                _logger.LogInformation($"Golem status update. {v}");
-                await statusChannel.Writer.WriteAsync(v);
-            };
-            golem.PropertyChanged += new PropertyChangedHandler<Golem, GolemStatus>(nameof(IGolem.Status), golemStatus).Subscribe();
+            Channel<GolemStatus> golemStatusChannel = PropertyChangeChannel(golem, nameof(IGolem.Status), 
+                (GolemStatus v) => _logger.LogInformation($"Golem status update: {v}"));
 
-            var jobChannel = Channel.CreateUnbounded<IJob?>();
-            Action<IJob?> currentJobHook = async (v) =>
-            {
-                _logger.LogInformation($"Current Job update. {v}");
-                await jobChannel.Writer.WriteAsync(v);
-            };
-            golem.PropertyChanged += new PropertyChangedHandler<Golem, IJob?>(nameof(IGolem.CurrentJob), currentJobHook).Subscribe();
+            Channel<IJob> jobChannel = PropertyChangeChannel(golem, nameof(IGolem.CurrentJob), 
+                (IJob? v) => _logger.LogInformation($"Current Job update: {v}"));
+
+
+            // Then
+
+            // 
+            // Starting Golem
+            // 
+
+            // Golem status is `Off` before start.
+            Assert.Equal(GolemStatus.Off, golem.Status);
+
+            // `CurrentJob` before startup should be null.
+            Assert.Null(golem.CurrentJob);
 
             _logger.LogInformation("Starting Golem");
             await golem.Start();
-            Assert.Equal(GolemStatus.Starting, await statusChannel.Reader.ReadAsync());
+            // On startup Golem status goes from `Off` to `Starting`
+            Assert.Equal(GolemStatus.Starting, await SkipMatching(golemStatusChannel, (GolemStatus s) => s == GolemStatus.Off));
 
-            var readyStatus = await SkipMatching(statusChannel, (GolemStatus status) => { return status == GolemStatus.Starting; });
-            Assert.Equal(GolemStatus.Ready, readyStatus);
+            // .. and then to `Ready`
+            Assert.Equal(GolemStatus.Ready, await SkipMatching(golemStatusChannel, (GolemStatus s) => s == GolemStatus.Starting));
 
+            // `CurrentJob` after startup, before taking any Job should be null
             Assert.Null(golem.CurrentJob);
 
-            _logger.LogInformation("Starting App");
+            //
+            // Starting Sample App
+            // 
+
+            _logger.LogInformation("Starting Sample App");
             var app = _requestor?.CreateSampleApp() ?? throw new Exception("Requestor not started yet");
             Assert.True(app.Start());
 
-            IJob? donwloadingCurrentJob = await SkipMatching(jobChannel, (IJob? j) => { return j?.Status == JobStatus.Idle; });
-            Assert.Equal(JobStatus.DownloadingModel, donwloadingCurrentJob?.Status);
+            // `CurrentJob` property update notification.
+            IJob? currentJob = await SkipMatching(jobChannel, (IJob? j) => { return j == null; });
+            // `CurrentJob` object property and object arriving as a property notification are the same.
+            Assert.Same(currentJob, golem.CurrentJob);
+            Assert.NotNull(currentJob);
+
+            // Creating property update channels for `CurrentJob` properties.
+
+            Channel<JobStatus> jobStatusChannel = PropertyChangeChannel(currentJob, nameof(currentJob.Status), 
+                (JobStatus v) => _logger.LogInformation($"Current job Status update: {v}"));
+            Channel<GolemLib.Types.PaymentStatus> jobPaymentStatusChannel = PropertyChangeChannel(currentJob, nameof(currentJob.PaymentStatus), 
+                (GolemLib.Types.PaymentStatus v) => _logger.LogInformation($"Current job Payment Status update: {v}"));
+            Channel<GolemUsage> jobGolemUsageChannel = PropertyChangeChannel(currentJob, nameof(currentJob.CurrentUsage), 
+                (GolemUsage v) => _logger.LogInformation($"Current job Usage update: {v}"));
+            Channel<Payment> jobPaymentConfirmationChannel = PropertyChangeChannel(currentJob, nameof(currentJob.PaymentConfirmation), 
+                (Payment v) => _logger.LogInformation($"Current job Payment Confirmation update: {v}"));
+
+            // // Job starts with `Idle` status and transitions to `DownloadingModel`
+            Assert.Equal(JobStatus.DownloadingModel, await SkipMatching(jobStatusChannel, (JobStatus s) => s == JobStatus.Idle));
+            // Then it transitions into `Computing`.
+            Assert.Equal(JobStatus.Computing, await SkipMatching(jobStatusChannel, (JobStatus s) => s == JobStatus.DownloadingModel));
+            // Status is the same on `currentJob` object.
+            Assert.Equal(JobStatus.Computing, currentJob?.Status);
+            // Original `currentJob` and `golem.CurrentJob` are still same objects.
+            Assert.Same(currentJob, golem.CurrentJob);
+
             IJob? computingCurrentJob = await SkipMatching(jobChannel, (IJob? j) => { return j?.Status == JobStatus.DownloadingModel; });
-            Assert.Equal(JobStatus.Computing, donwloadingCurrentJob?.Status);
+            Assert.Equal(JobStatus.Computing, currentJob?.Status);
 
             _logger.LogInformation($"Got a job. Status {golem.CurrentJob?.Status}, Id: {golem.CurrentJob?.Id}, RequestorId: {golem.CurrentJob?.RequestorId}");
 
             Assert.NotNull(golem.CurrentJob);
             Assert.Equal(golem.CurrentJob.RequestorId, _requestor?.AppKey);
             Assert.Equal(golem.CurrentJob?.Status, JobStatus.Computing);
+
+            // 
 
             _logger.LogInformation("Stopping App");
             await app.Stop(StopMethod.SigInt);
@@ -124,10 +162,14 @@ namespace Golem.Tests
             _logger.LogInformation("Stopping Golem");
             await golem.Stop();
 
-            var offStatus = await SkipMatching(statusChannel, (GolemStatus status) => { return status == GolemStatus.Ready; });
+            var offStatus = await SkipMatching(golemStatusChannel, (GolemStatus status) => { return status == GolemStatus.Ready; });
             Assert.Equal(GolemStatus.Off, offStatus);
         }
 
+        /// <summary>
+        /// Reads from `channel` and returns first `T` for which `matcher` returns `false`
+        /// </summary>
+        /// <exception cref="Exception">Thrown when reading channel exceeds in total `timeoutMs`</exception>
         public async Task<T> SkipMatching<T>(ChannelReader<T> channel, Func<T, bool> matcher, double timeoutMs = 10_000)
         {
             var cancelTokenSource = new CancellationTokenSource();
@@ -145,6 +187,22 @@ namespace Golem.Tests
             }
 
             throw new Exception($"Failed to find matching {nameof(T)} within {timeoutMs} ms.");
+        }
+
+        /// <summary>
+        /// Creates channel of updated properties.
+        /// `extraHandler` is invoked each time property arrives.
+        /// </summary>
+        public Channel<T> PropertyChangeChannel<OBJ, T>(OBJ notifiable, string propName, Action<T?>? extraHandler = null) where OBJ: INotifyPropertyChanged
+        {
+            var eventChannel = Channel.CreateUnbounded<T>();
+            Action<T?> emitEvent = async (v) =>
+            {
+                extraHandler?.Invoke(v);
+                await eventChannel.Writer.WriteAsync(v);
+            };
+            notifiable.PropertyChanged += new PropertyChangedHandler<Golem, T>(propName, emitEvent).Subscribe();
+            return eventChannel;
         }
 
         public async Task DisposeAsync()
