674f910fda0856c32fcb83f19bcb185c91c865d4
Przemyslaw Walski <pwalski@users.noreply.github.com>
2024-01-12 13:03:59 +0100

    GolemTests logs. Logs in gh action package. Serial tests execution.

--- Golem.Tests/GlobalUsings.cs
+++ Golem.Tests/GlobalUsings.cs
@@ -1,1 +1,23 @@
 global using Xunit;
+[assembly: CollectionBehavior(
+    CollectionBehavior.CollectionPerClass,
+    DisableTestParallelization = true,
+    MaxParallelThreads = 0
+)]
+
+[CollectionDefinition(nameof(SerialTestCollection), DisableParallelization = true)]
+public class SerialTestCollection { }
+
+public class GolemFixture : IDisposable
+{
+    public GolemFixture(IMessageSink sink)
+    {
+        Sink = sink;
+    }
+
+    public IMessageSink Sink { get; }
+
+    public void Dispose()
+    {
+    }
+}
