b1f5ee392607d360760b9637f31902208600dfe0
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2024-02-02 17:42:18 +0100

    Fix model hash; remove unnecessary properties

--- example/ai-requestor/ai_runtime.py
+++ example/ai-requestor/ai_runtime.py
@@ -218,8 +218,7 @@ class ProviderOnceStrategy(MarketStrategy):
 # App
 
 RUNTIME_NAME = "automatic" 
-# RUNTIME_NAME = "dummy"
-CAPABILITIES = "golem.runtime.capabilities"
+#RUNTIME_NAME = "dummy"
 
 @dataclass
 class AiPayload(Payload):
@@ -227,8 +226,6 @@ class AiPayload(Payload):
     image_fmt: str = prop("golem.!exp.ai.v1.srv.comp.ai.model-format", default="safetensors")
 
     runtime: str = constraint(inf.INF_RUNTIME_NAME, default=RUNTIME_NAME)
-    capabilities: str = constraint(CAPABILITIES, default="automatic")
-    # capabilities: str = constraint(CAPABILITIES, default="dummy")
 
 
 class AiRuntimeService(Service):
@@ -237,7 +234,7 @@ class AiRuntimeService(Service):
         ## TODO switched into using smaller model to avoid problems during tests. Resolve it when automatic runtime integrated
         # return AiPayload(image_url="hash:sha3:92180a67d096be309c5e6a7146d89aac4ef900e2bf48a52ea569df7d:https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors?download=true")
         # return AiPayload(image_url="hash:sha3:0b682cf78786b04dc108ff0b254db1511ef820105129ad021d2e123a7b975e7c:https://huggingface.co/cointegrated/rubert-tiny2/resolve/main/model.safetensors?download=true")
-        return AiPayload(image_url="hash:sha3:6ce0161689b3853acaa03779ec93eafe75a02f4ced659bee03f50797806fa2fa:https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.safetensors?download=true")
+        return AiPayload(image_url="hash:sha3:b2da48d618beddab1887739d75b50a3041c810bc73805a416761185998359b24:https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.safetensors?download=true")
     
     
     async def start(self):
