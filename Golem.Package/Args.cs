ccfbc13300f7f0e0b7a41bb843beebabef97b026
Przemyslaw Walski <pwalski@users.noreply.github.com>
2024-02-02 16:25:26 +0100

    Golem version pre-rel-v0.15.0-rc14_gsb_proxy ya-runtime-ai version pre-rel-v0.1.0-rc28

--- Golem.Package/Args.cs
+++ Golem.Package/Args.cs
@@ -5,9 +5,9 @@ public class BuildArgs
 {
     [Option('t', "target", Default = "package", Required = false, HelpText = "Directory where binaries will be generated relative to working dir")]
     public required string Target { get; set; }
-    [Option('y', "yagna-version", Default = "pre-rel-v0.15.0-rc13_gsb_proxy", Required = false, HelpText = "Yagna version github tag")]
+    [Option('y', "yagna-version", Default = "pre-rel-v0.15.0-rc14_gsb_proxy", Required = false, HelpText = "Yagna version github tag")]
     public required string GolemVersion { get; set; }
-    [Option('r', "runtime-version", Default = "pre-rel-v0.1.0-rc27", Required = false, HelpText = "Runtime version github tag")]
+    [Option('r', "runtime-version", Default = "pre-rel-v0.1.0-rc28", Required = false, HelpText = "Runtime version github tag")]
     public required string RuntimeVersion { get; set; }
     [Option('c', "dont-clean", Default = false, Required = false, HelpText = "Remove temporary directories")]
     public required bool DontClean { get; set; }
