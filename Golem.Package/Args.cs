75f0a7367083eb536dc208b87d42342be1b77290
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2024-02-26 17:43:52 +0100

    Update builds versions

--- Golem.Package/Args.cs
+++ Golem.Package/Args.cs
@@ -5,9 +5,9 @@ public class BuildArgs
 {
     [Option('t', "target", Default = "package", Required = false, HelpText = "Directory where binaries will be generated relative to working dir")]
     public required string Target { get; set; }
-    [Option('y', "yagna-version", Default = "pre-rel-v0.15.0-rc16_gsb_proxy", Required = false, HelpText = "Yagna version github tag")]
+    [Option('y', "yagna-version", Default = "pre-rel-v0.16.0-ai-rc6", Required = false, HelpText = "Yagna version github tag")]
     public required string GolemVersion { get; set; }
-    [Option('r', "runtime-version", Default = "pre-rel-v0.1.0-rc29", Required = false, HelpText = "Runtime version github tag")]
+    [Option('r', "runtime-version", Default = "v0.1.0", Required = false, HelpText = "Runtime version github tag")]
     public required string RuntimeVersion { get; set; }
     [Option('c', "dont-clean", Default = false, Required = false, HelpText = "Remove temporary directories")]
     public required bool DontClean { get; set; }
@@ -15,7 +15,7 @@ public class BuildArgs
     public required string? DllDir { get; set; }
     [Option('p', "dll-file-patterns", Default = "Golem.dll,GolemLib.dll", Required = false, HelpText = "Coma separated dll file name patterns")]
     public required string DllFilePatterns { get; set; }
-    
+
 }
 
 [Verb("download", HelpText = "Downloads package from public repository.")]
