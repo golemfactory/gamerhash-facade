b7018897c4562c01948090187940704e32df3ca1
Staszek Krotki <16387248+staszek-krotki@users.noreply.github.com>
2024-03-27 11:06:22 +0100

    update yagna

--- Golem.Tools/GolemPackageBuilder.cs
+++ Golem.Tools/GolemPackageBuilder.cs
@@ -18,7 +18,7 @@ namespace Golem.Tools
 {
     public class PackageBuilder
     {
-        public static string CURRENT_GOLEM_VERSION = "pre-rel-v0.16.0-ai-rc7";
+        public static string CURRENT_GOLEM_VERSION = "pre-rel-v0.16.0-ai-rc8";
         public static string CURRENT_RUNTIME_VERSION = "v0.1.1";
 
         internal static string InitTestDirectory(string name, bool cleanupData = true)
