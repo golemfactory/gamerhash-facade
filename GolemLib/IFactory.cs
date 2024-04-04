1f0592becd5531ed574dfa4d5bd7fb298bc6504d
Przemysław Walski <pwalski@users.noreply.github.com>
2024-04-04 16:25:52 +0200

    IFactory code comment docs

--- GolemLib/IFactory.cs
+++ GolemLib/IFactory.cs
@@ -4,5 +4,11 @@ using Microsoft.Extensions.Logging;
 
 public interface IFactory
 {
+    /// <summary>Creates Golem object</summary>
+    /// <param name="mainnet">Enables usage of mainnet payment network. By default `true`</param>
+    /// <param name="dataDir">Path where `provider` and `yagna` data directories will be created.
+    /// If not specified it will default to `golem-data` dir inside of Golem package directory.
+    /// </param>
+    /// <returns>Golem object</returns>
     public Task<IGolem> Create(string modulesDir, ILoggerFactory? loggerFactory = null, bool mainnet = true, string? dataDir = null);
 }
