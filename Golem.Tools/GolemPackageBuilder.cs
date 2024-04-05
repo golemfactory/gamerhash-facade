e9233b23344c789129ad0fb6d80d985c581e1f17
Przemysław Walski <pwalski@users.noreply.github.com>
2024-04-05 16:06:53 +0200

    ya-runtime-ai version pre-rel-v0.2.0-rc11 (statically linked VC redist. Configurable data dir.)

--- Golem.Tools/GolemPackageBuilder.cs
+++ Golem.Tools/GolemPackageBuilder.cs
@@ -19,7 +19,7 @@ namespace Golem.Tools
     public class PackageBuilder
     {
         public static string CURRENT_GOLEM_VERSION = "pre-rel-v0.16.0-ai-rc12";
-        public static string CURRENT_RUNTIME_VERSION = "pre-rel-v0.2.0-rc9";
+        public static string CURRENT_RUNTIME_VERSION = "pre-rel-v0.2.0-rc11";
 
         internal static string InitTestDirectory(string name, bool cleanupData = true)
         {
