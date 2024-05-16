0fff6057c8a9f1e0cd19cfe5a8eeef7cbebf728d
Przemysław Walski <pwalski@users.noreply.github.com>
2024-05-16 14:50:43 +0200

    Use ya-runtime-ai pre-rel-v0.2.2-rc1 (Dummy runtime with statically linked VCRuntime)

--- Golem.Tools/GolemPackageBuilder.cs
+++ Golem.Tools/GolemPackageBuilder.cs
@@ -19,7 +19,7 @@ namespace Golem.Tools
     public class PackageBuilder
     {
         public static string CURRENT_GOLEM_VERSION = "pre-rel-v0.16.0-ai-rc16";
-        public static string CURRENT_RUNTIME_VERSION = "v0.2.1";
+        public static string CURRENT_RUNTIME_VERSION = "pre-rel-v0.2.2-rc1";
 
         internal static string InitTestDirectory(string name, bool cleanupData = true)
         {
