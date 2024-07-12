0d4e990392c8cae2fba22a9f0318ababe45e8e06
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2024-07-12 17:27:41 +0200

    Test scenarios with termination race conditions

--- Golem.Tests/JobStatusTests.cs
+++ Golem.Tests/JobStatusTests.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Text.RegularExpressions;
 using System.Threading.Channels;
 
+using Golem.Model;
 using Golem.Tools;
 using Golem.Yagna.Types;
 
@@ -137,7 +138,7 @@ namespace Golem.Tests
             await Task.Delay(2 * 1000);
 
             _logger.LogInformation("=================== Terminating App ===================");
-            var rest = _requestor.Rest != null ? _requestor.Rest : throw new Exception("Rest api on Requestor not initialized.");
+            var rest = _requestor.Rest ?? throw new Exception("Rest api on Requestor not initialized.");
             var activities = await rest.GetActivities(currentJob.Id);
 
             // Kill script so he doesn't have chance to handle tasks termination.
@@ -340,6 +341,163 @@ namespace Golem.Tests
 
             // Stopping Golem
             await StopGolem(golem, golemPath, StatusChannel(golem));
+        }
+
+        [Fact]
+        public async Task ProviderRestart_RequestorBreaksAgreement()
+        {
+            string golemPath = await PackageBuilder.BuildTestDirectory();
+            await using var golem = (Golem)await TestUtils.Golem(golemPath, _loggerFactory, null, RelayType.Local);
+            _logger.LogInformation($"Path: {golemPath}");
+
+            await StartGolem(golem, StatusChannel(golem));
+            var jobChannel = JobChannel(golem);
+
+            _logger.LogInformation("=================== Starting Sample App ===================");
+            await using var app = _requestor?.CreateSampleApp() ?? throw new Exception("Requestor not started yet");
+            Assert.True(app.Start());
+
+            // Wait for job.
+            Job? currentJob = await ReadChannel<Job?>(jobChannel);
+            Assert.NotNull(currentJob);
+
+            var jobStatusChannel = JobStatusChannel(currentJob);
+            var agreementId = currentJob.Id;
+
+            // Wait until ExeUnit will be created.
+            // Workaround for situations, when status update was so fast, that we were not able to create
+            // channel yet, so waiting for update would be pointless.
+            if (currentJob.Status != JobStatus.Computing)
+                Assert.Equal(JobStatus.Computing, await ReadChannel(jobStatusChannel,
+                    (JobStatus s) => s == JobStatus.DownloadingModel || s == JobStatus.Idle));
+            // Let him compute for a while.
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            _logger.LogInformation("=================== Killing Provider Agent ===================");
+            // Provider is killed so he is not able to terminate Agreement.
+            // Yagna has chance to be closed gracefully and close net connection with Requestor.
+            // If we would double Stop here, re-establishing connection would last longer.
+            var golemStatusChannel = StatusChannel(golem);
+            var pid = golem.GetProviderPid();
+            if (pid.HasValue)
+                Process.GetProcessById(pid.Value).Kill();
+
+            _logger.LogInformation("=================== Killing App ===================");
+            // Note: We need to manually send termination from Requestor with correct Reason.
+            // It's better to avoid yapapi finding out that Provider stopped working, because it
+            // could take action otherwise.
+            await app.Stop(StopMethod.SigKill);
+
+            await AwaitValue<JobStatus>(jobStatusChannel, JobStatus.Interrupted, TimeSpan.FromSeconds(30));
+            Assert.Equal(JobStatus.Interrupted, currentJob.Status);
+            await AwaitValue<Job?>(jobChannel, null, TimeSpan.FromSeconds(1));
+            Assert.Null(golem.CurrentJob);
+
+            await AwaitValue(golemStatusChannel, GolemStatus.Error, TimeSpan.FromSeconds(3));
+
+            _logger.LogInformation("=================== Restart Provider Agent ===================");
+            await golem.Start();
+
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            Assert.Equal(JobStatus.Interrupted, currentJob.Status);
+
+            _logger.LogInformation("=================== Requestor terminates Agreement ===================");
+            var rest = _requestor.Rest ?? throw new Exception("Rest api on Requestor not initialized.");
+            var reason = new Reason(null!, "Healthcheck failed");
+            reason.RequestorCode = "HealthCheckFailed";
+            await rest.TerminateAgreement(agreementId, reason);
+
+            // Wait for propagation of termination event
+            await Task.Delay(TimeSpan.FromSeconds(2));
+
+            Assert.Equal(JobStatus.Interrupted, currentJob.Status);
+        }
+
+        [Fact]
+        public async Task ProviderRestart_ProviderBreaksAgreement()
+        {
+            string golemPath = await PackageBuilder.BuildTestDirectory();
+            await using var golem = (Golem)await TestUtils.Golem(golemPath, _loggerFactory, null, RelayType.Local);
+            _logger.LogInformation($"Path: {golemPath}");
+
+            await StartGolem(golem, StatusChannel(golem));
+            var jobChannel = JobChannel(golem);
+
+            _logger.LogInformation("=================== Starting Sample App ===================");
+            await using var app = _requestor?.CreateSampleApp() ?? throw new Exception("Requestor not started yet");
+            Assert.True(app.Start());
+
+            // Wait for job.
+            Job? currentJob = await ReadChannel<Job?>(jobChannel);
+            Assert.NotNull(currentJob);
+
+            var jobStatusChannel = JobStatusChannel(currentJob);
+            var agreementId = currentJob.Id;
+
+            // Wait until ExeUnit will be created.
+            // Workaround for situations, when status update was so fast, that we were not able to create
+            // channel yet, so waiting for update would be pointless.
+            if (currentJob.Status != JobStatus.Computing)
+                Assert.Equal(JobStatus.Computing, await ReadChannel(jobStatusChannel,
+                    (JobStatus s) => s == JobStatus.DownloadingModel || s == JobStatus.Idle));
+            // Let him compute for a while.
+            await Task.Delay(2 * 1000);
+
+            _logger.LogInformation("=================== Killing Provider Agent ===================");
+            // Provider is killed so he is not able to terminate Agreement.
+            // Yagna has chance to be closed gracefully and close net connection with Requestor.
+            // If we would double Stop here, re-establishing connection would last longer.
+            var golemStatusChannel = StatusChannel(golem);
+            var pid = golem.GetProviderPid();
+            if (pid.HasValue)
+                Process.GetProcessById(pid.Value).Kill();
+
+            _logger.LogInformation("=================== Killing App ===================");
+            // Note: Provider needs to send termination with correct Reason.
+            // It's better to avoid yapapi finding out that Provider stopped working, because it
+            // could take action otherwise.
+            await app.Stop(StopMethod.SigKill);
+
+            await AwaitValue<JobStatus>(jobStatusChannel, JobStatus.Interrupted, TimeSpan.FromSeconds(30));
+            Assert.Equal(JobStatus.Interrupted, currentJob.Status);
+            await AwaitValue<Job?>(jobChannel, null, TimeSpan.FromSeconds(1));
+            Assert.Null(golem.CurrentJob);
+
+            await AwaitValue(golemStatusChannel, GolemStatus.Error, TimeSpan.FromSeconds(3));
+
+            _logger.LogInformation("=================== Restart Provider Agent ===================");
+            await golem.Start();
+
+            Assert.Null(golem.CurrentJob);
+
+            // ListJobs is one of things that can trigger termination of interrupted job.
+            // This will happen as well, when new event from yagna API will be processed.
+            var jobs = await golem.ListJobs(DateTime.Now - TimeSpan.FromMinutes(1));
+            var prevJob = jobs.Find(job => job.Id == agreementId) as Job;
+
+            Assert.NotNull(prevJob);
+            Assert.Equal(JobStatus.Interrupted, prevJob.Status);
+
+            _logger.LogInformation("=================== Requestor terminates Agreement ===================");
+            // Try to terminate Agreement with Reason which in normal situation would be treated as correct termination
+            // resulting in Finished status. If Provider didn't terminate Agreement with Reason leading to Interrupted
+            // state, then JobStatus would be restored to Finished and it would reveal error.
+            var rest = _requestor.Rest ?? throw new Exception("Rest api on Requestor not initialized.");
+            var reason = new Reason(null!, "Agreement is no longer needed");
+            reason.RequestorCode = "NoLongerNeeded";
+            try
+            {
+                // Termination should fail, because Provider already terminated Agreement.
+                await rest.TerminateAgreement(agreementId, reason);
+            }
+            catch (Exception e)
+            {
+                _logger.LogInformation(e, "Failed to terminate agreement");
+            }
+
+            // Wait for propagation of termination event. Status should be still Interrupted.
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            Assert.Equal(JobStatus.Interrupted, prevJob.Status);
         }
     }
 }
