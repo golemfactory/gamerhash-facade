99b2e9e10e61b2f71264abdb5a4e08d1eaad222b
Mateusz Walkiewicz <mwalkiewicz@unoperate.com>
2024-07-29 13:22:30 +0200

    Bump yagna

--- Golem.Package/Args.cs
+++ Golem.Package/Args.cs
@@ -5,7 +5,7 @@ public class BuildArgs
 {
     [Option('t', "target", Default = "package", Required = false, HelpText = "Directory where binaries will be generated relative to working dir")]
     public required string Target { get; set; }
-    [Option('y', "yagna-version", Default = "pre-rel-v0.16.0-preview.ai.24", Required = false, HelpText = "Yagna version github tag")]
+    [Option('y', "yagna-version", Default = "pre-rel-v0.16.0-preview.ai.28", Required = false, HelpText = "Yagna version github tag")]
     public required string GolemVersion { get; set; }
     [Option('r', "runtime-version", Default = "v0.2.3", Required = false, HelpText = "Runtime version github tag")]
     public required string RuntimeVersion { get; set; }
