5c602bb0502244eaae342e552ed844d11393fe5d
Mateusz Walkiewicz <mwalkiewicz@unoperate.com>
2024-08-06 13:53:57 +0200

    wip

--- scripts/automatic.ps1
+++ scripts/automatic.ps1
@@ -1,5 +1,5 @@
 param (
-    [string]$automatic_runtime_package_url = "https://gpu-on-golem.s3.eu-central-1.amazonaws.com/sd-webui-standalone/sd-webui-gh-pre-rel-v0.2.5-rc2.zip",
+    [string]$automatic_runtime_package_url = "https://gpu-on-golem.s3.eu-central-1.amazonaws.com/sd-webui-standalone/sd-webui-gh-pre-rel-v0.2.5-rc3.zip",
     [string]$automatic_package_dir = "package",
     [bool]$compress = 0,
     [bool]$cleanup = 0
