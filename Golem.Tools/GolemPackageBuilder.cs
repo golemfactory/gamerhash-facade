1819ed1a671b3cde7479c815e5c64c72eb99966b
Mateusz Walkiewicz <mwalkiewicz@unoperate.com>
2024-10-03 16:45:48 +0200

    Bump ya-runtime-ai to v0.2.7

--- Golem.Tools/GolemPackageBuilder.cs
+++ Golem.Tools/GolemPackageBuilder.cs
@@ -19,7 +19,7 @@ namespace Golem.Tools
     public class PackageBuilder
     {
         public static string CURRENT_GOLEM_VERSION = "pre-rel-v0.16.0-preview.ai.49";
-        public static string CURRENT_RUNTIME_VERSION = "v0.2.6";
+        public static string CURRENT_RUNTIME_VERSION = "v0.2.7";
 
         internal static string InitTestDirectory(string name, bool cleanupData = true)
         {
